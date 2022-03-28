a284a945d055015b484ed4393416e4c0b44e487d
Meera <60855420+meera-gopi@users.noreply.github.com>
2022-03-28 22:30:31 +0530

    Update DataApiApplication.java

--- src/main/java/com/medicare/DataApi/DataApiApplication.java
+++ src/main/java/com/medicare/DataApi/DataApiApplication.java
@@ -3,6 +3,8 @@ package com.medicare.DataApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -18,7 +20,12 @@ import com.medicare.repository.ProductRepository;
 @EntityScan("com.medicare.Entity")
 @EnableJpaRepositories(basePackageClasses= {CustomerRepository.class,EmployeeRepository.class,OrderItemsRepository.class,OrderRepository.class,OrderStatusRepository.class,ProductCategoryRepository.class,ProductRepository.class})
 @ComponentScan(basePackages= {"com.medicare.Entity","com.medicare.repository","com.medicare.service","com.medicare.service.impl","com.medicare.controller","com.medicare.DTO"})
-public class DataApiApplication {
+public class DataApiApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DataApiApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataApiApplication.class, args);
